ee32ff904f361c293a6ccc842279f4c851b3428d
phuongthe182 <136964428+phuongthe182@users.noreply.github.com>
2024-08-02 13:44:08 +0000

    last update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xd54C727740dFcf9e5D0dD306Fe93826F18524B04'
+export const ProxyAdmin = '0xf196522d08dE2f1710a3D8e1520c39Ae0Fdf21f6'
+export const SWTRImplementation = '0x08176642171E9D442dF4BB92995A21EFA0700592'
     
